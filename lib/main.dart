3ceff142aa2a2d3629d832b775e1a868efda590c
sojung <sojung528@gmail.com>
2023-04-14 14:55:11 +0900

    dev 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
+
